3ccc6f41eec221d517d317ad8bebba2a1ad02bef
Super Yoshi 10000 <SuperYoshi10000@gmail.com>
2025-08-28 22:37:32 -0400

    add basic layout and information

---
+++ src/routes/details/+page.svelte
@@ -0,0 +1,24 @@
+<div class="w-1/2 mx-auto">
+    <h1 class="font-bold text-4xl text-center">Details</h1>
+    <section>
+        <h2 class="text-2xl">Requirements</h2>
+        <ul>
+            <li class="indent-5.5">Must be math related</li>
+        </ul>
+    </section>
+    <section>
+        <h2 class="text-2xl">Rewards</h2>
+        You must spend at least:
+        <ul>
+            <li class="indent-5.5"><u>5 hours</u> for a <i>scientific calculator</i>.</li>
+            <li class="indent-5.5"><u>20 hours</u> for a <u>graphing calculator</u>.</li>
+        </ul>
+    </section>
+    <section>
+        <h2 class="text-2xl">Questions</h2>
+        <dl>
+            <dt class="indent-5.5 font-bold">Is this double-dippable with Summer of Making?</dt>
+            <dd class="indent-5.5">Maybe</dd>
+        </dl>
+    </section>
+</div>
